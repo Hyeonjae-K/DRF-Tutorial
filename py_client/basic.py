06cb369e54c909eedcfe2f7f4d0dfd8798a29052
Hyeonjae-K <href.hjk@gmail.com>
2022-09-05 22:21:41 +0900

    Create first API View

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,6 +1,6 @@
 import requests
 
-endpoint = 'https://httpbin.org/anything'
+endpoint = 'http://127.0.0.1:8000/api/'
 
 get_response = requests.get(
     endpoint, json={'query': 'Hello world'})  # HTTP Request
@@ -9,5 +9,5 @@ print(get_response.text)  # print raw text response
 # HTTP Reqeust -> HTML
 # REST API HTTP Request -> JSON
 # JavaScript Object Notation ~ Python Dict
-print(get_response.json())
+print(get_response.json()['message'])
 print(get_response.status_code)
