946535c66eb4d16f84968a383db62e89cb123b3e
Hyeonjae-K <href.hjk@gmail.com>
2022-09-06 00:04:37 +0900

    Echo GET Data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,28 @@
+import json
+
 from django.http import JsonResponse
 
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hi there, this is your Django API response!!"})
+    # request -> HttpResponse -> Django
+    # print(dir(request))
+    #  request.body
+    body = request.body  # byte string of JSON data
+
+    data = {}
+    try:
+        data = json.loads(body)  # string of JSON data -> Python Dict
+    except Exception:
+        pass
+    print(type(data), data)
+    print(request.headers)
+    print(request.content_type)
+
+    print(request.GET)  # url query params
+    # print(request.POST)
+
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -3,11 +3,12 @@ import requests
 endpoint = 'http://127.0.0.1:8000/api/'
 
 get_response = requests.get(
-    endpoint, json={'query': 'Hello world'})  # HTTP Request
-print(get_response.text)  # print raw text response
+    endpoint, params={'abc': 123}, json={'query': 'Hello world'})  # HTTP Request
+# print(get_response.text)  # print raw text response
+print(get_response.headers)
 
 # HTTP Reqeust -> HTML
 # REST API HTTP Request -> JSON
 # JavaScript Object Notation ~ Python Dict
-print(get_response.json()['message'])
+print(get_response.json())
 print(get_response.status_code)
