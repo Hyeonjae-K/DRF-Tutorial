e098c1a2b06ce99d42ad1eefbf9c4ddac1ff95fe
Hyeonjae-K <href.hjk@gmail.com>
2022-09-06 23:27:58 +0900

    Django Model Instance as API Response

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,27 +2,21 @@ import json
 
 from django.http import JsonResponse
 
+from products.models import Product
+
 
 def api_home(request, *args, **kwargs):
-    # request -> HttpResponse -> Django
-    # print(dir(request))
-    #  request.body
-    body = request.body  # byte string of JSON data
-
+    model_data = Product.objects.all().order_by('?').first()
     data = {}
-    try:
-        data = json.loads(body)  # string of JSON data -> Python Dict
-    except Exception:
-        pass
-    print(type(data), data)
-    print(request.headers)
-    print(request.content_type)
 
-    print(request.GET)  # url query params
-    # print(request.POST)
-
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
+    # serialization
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
+        # model instance (model_data)
+        # turn a Python dict
+        # return JSON to my client
 
     return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -5,7 +5,7 @@ endpoint = 'http://127.0.0.1:8000/api/'
 get_response = requests.get(
     endpoint, params={'abc': 123}, json={'query': 'Hello world'})  # HTTP Request
 # print(get_response.text)  # print raw text response
-print(get_response.headers)
+# print(get_response.headers)
 
 # HTTP Reqeust -> HTML
 # REST API HTTP Request -> JSON
