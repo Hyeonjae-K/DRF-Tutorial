408bc47a36997c17a15c93967249d93e2d24c938
Hyeonjae-K <href.hjk@gmail.com>
2022-09-07 23:11:30 +0900

    Using Function Based Views For Create Retrieve or List

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,8 @@
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from django.shortcuts import get_object_or_404
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -24,12 +28,46 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     serializer_class = ProductSerializer
 
 
-class ProductListAPIView(generics.ListAPIView):
-    """
-    Not gonna use this method
-    """
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+# class ProductListAPIView(generics.ListAPIView):
+#     """
+#     Not gonna use this method
+#     """
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
 
-# product_detail_view = ProductDetailAPIView.as_view()
-# product_create_view = ProductCreateAPIView.as_view()
+
+# adds confusion.
+# this is why using generic views
+@api_view(['GET', 'POST'])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method
+
+    if method == 'GET':
+        if pk is not None:
+            # detail view
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, many=False).data
+            return Response(data)
+        else:
+            # list view
+            queryset = Product.objects.all()
+            data = ProductSerializer(queryset, many=True).data
+            return Response(data)
+
+    if method == 'POST':
+        # create an item
+        serializer = ProductSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content') or None
+
+            if content is None:
+                content = title
+
+            serializer.save(content=content)
+            return Response(serializer.data)
+        return Response({'invalid': 'not good data'}, status=400)
+
+    # product_detail_view = ProductDetailAPIView.as_view()
+    # product_create_view = ProductCreateAPIView.as_view()
