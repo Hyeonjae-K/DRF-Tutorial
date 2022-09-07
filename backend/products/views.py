37fa051ff032b4ff2736b97d31ade6e1a3f3d842
Hyeonjae-K <href.hjk@gmail.com>
2022-09-07 21:40:48 +0900

    Django Rest Framework CreateAPIView

--- backend/products/views.py
+++ backend/products/views.py
@@ -7,9 +7,24 @@ from .models import Product
 from .serializers import ProductSerializer
 
 
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+        print(serializer.validated_data)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
+
+
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
 # product_detail_view = ProductDetailAPIView.as_view()
+# product_create_view = ProductCreateAPIView.as_view()
